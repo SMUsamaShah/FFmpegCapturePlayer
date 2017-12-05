05623daf43b998ae747a7f125d72dba837742ab1
Usama <smusamashah@gmail.com>
2017-12-05 17:06:37 +0500

    Common methods

---
+++ FFmpegPlayer/Common.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdDetectVideoPlayer
+{
+    static class Common
+    {
+        public static void InvokeUI(Control ctrl, Action action)
+        {
+            if (ctrl.InvokeRequired)
+            {
+                ctrl.Invoke((MethodInvoker)delegate { InvokeUI(ctrl, action); });
+                return;
+            }
+            action();
+        }
+    }
+}
