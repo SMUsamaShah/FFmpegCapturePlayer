484ff4424c43080a20c6fce376587dfd76bc82a0
Usama <smusamashah@gmail.com>
2017-12-06 15:08:12 +0500

    Added custom user control with a PictureBox to show associated VideoFrame and labels to show frame information

---
+++ FFmpegPlayer/Components/FrameViewer.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace AdDetectVideoPlayer.Components
+{
+    partial class FrameViewer
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(226, 165);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Location = new System.Drawing.Point(0, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 41);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Index: {0}, Timestamp: {2}";
+            // 
+            // FrameViewer
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "FrameViewer";
+            this.Size = new System.Drawing.Size(232, 212);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ FFmpegPlayer/Components/FrameViewer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdDetectVideoPlayer.Components
+{
+    public partial class FrameViewer : UserControl
+    {
+        private VideoFrame vframe;
+
+        public VideoFrame VideoFrame { get => vframe; }
+        public long SequenceNumber => vframe.SequenceNumber;
+
+        public event EventHandler Click;
+
+        public FrameViewer()
+        {
+            InitializeComponent();
+        }
+
+        public FrameViewer(VideoFrame vframe) : this()
+        {
+            this.vframe = vframe;
+            pictureBox.InvokeUI(() => {
+                pictureBox.Image = vframe.Bitmap;
+
+                label1.Text = String.Format(
+                    "Index: {0}, Timestamp: {1}",
+                    vframe.SequenceNumber,
+                    vframe.Timestamp);
+            });
+        }
+
+        public void SetVideoFrame(VideoFrame vframe)
+        {
+            this.vframe = vframe;
+            pictureBox.InvokeUI(() => {
+                pictureBox.Image = vframe.Bitmap;
+
+                label1.Text = String.Format(
+                    "Index: {0}, Timestamp: {1}",
+                    vframe.SequenceNumber,
+                    vframe.Timestamp);
+            });
+            
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            if (this.Click != null)
+                this.Click(this, e);
+        }
+    }
+}
